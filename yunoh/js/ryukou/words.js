e2e17e04cdc9766c20d68d6686c7e265dd51fb5b
kutakuta15 <kd1342624@st.kobedenshi.ac.jp>
2025-05-14 16:04:17 +0900

    ワードを外部に逃がしてnameで統一するように

---
+++ yunoh/js/ryukou/words.js
@@ -0,0 +1,16 @@
+const wordsList = {
+    d1: "あいうえお",
+    d2: "かきくけこ",
+    d3: "さしすせそ",
+    d4: "たちつてと",
+    d5: "なにぬねの"
+}
+
+for (let name in wordsList) {
+    let elements = document.getElementsByName(name);
+    for (let element of elements) {
+        element.innerHTML = wordsList[name];
+        element.setAttribute('class', 'word');
+        element.setAttribute('for', 'popup-flag');
+    }
+}
