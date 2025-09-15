317e3f4a5d9debfd7dd427263568904197634aaf
kutakuta15 <kutakuta15@users.noreply.github.com>
2025-09-15 21:27:21 +0900

    細かい修正を色々

--- WebGL/breakbricks/js/main.js
+++ WebGL/breakbricks/js/main.js
@@ -1,12 +1,16 @@
-const content = document.getElementById("content");
-const canvas = document.getElementById("game-canvas");
-const aspect = canvas.width / canvas.height;
+(() => {
+    const content = document.getElementById("content");
+    const canvas = document.getElementById("game-canvas");
+    /** @type {CanvasRenderingContext2D} */
+    const ctx = canvas.getContext("2d");
+    const aspect = canvas.width / canvas.height;
 
-window.addEventListener("resize", () => {
-    const availableWidth = content.clientWidth;
-    const displayWidth = availableWidth;
-    const displayHeight = displayWidth / aspect;
-    canvas.style.width = displayWidth + "px";
-    canvas.style.height = displayHeight + "px";
-});
-window.dispatchEvent(new Event("resize"));
+    window.addEventListener("resize", () => {
+        const availableWidth = content.clientWidth;
+        const displayWidth = availableWidth;
+        const displayHeight = displayWidth / aspect;
+        canvas.style.width = displayWidth + "px";
+        canvas.style.height = displayHeight + "px";
+    });
+    window.dispatchEvent(new Event("resize"));
+})();
--- yunoh/js/ryukou/memo.js
+++ yunoh/js/ryukou/memo.js
@@ -1,28 +1,30 @@
-// 保存データ
-let memoData = localStorage.getItem('memo');
-memoData = JSON.parse(memoData) || new Object();
+(() => {
+    // 保存データ
+    let memoData = localStorage.getItem('memo');
+    memoData = JSON.parse(memoData) || new Object();
 
-// メモ入力欄
-let popup_memo = document.querySelector('#text-memo');
+    // メモ入力欄
+    let popup_memo = document.querySelector('#text-memo');
 
-// 選択されているテキストを表示する場所
-let popup_selectText = document.getElementById('select-text');
+    // 選択されているテキストを表示する場所
+    let popup_selectText = document.getElementById('select-text');
 
-// 選択している要素
-let selected;
+    // 選択している要素
+    let selected;
 
-// 要素選択処理
-document.querySelectorAll('.word').forEach(element => {
-    element.addEventListener('click', () => {
-        selected = element;
-        popup_selectText.innerHTML = selected.innerHTML;
-        popup_memo.value = memoData[selected.getAttribute('name')] || "";
+    // 要素選択処理
+    document.querySelectorAll('.word').forEach(element => {
+        element.addEventListener('click', () => {
+            selected = element;
+            popup_selectText.innerHTML = selected.innerHTML;
+            popup_memo.value = memoData[selected.getAttribute('name')] || "";
+        });
     });
-});
 
-// 入力時保存処理
-popup_memo.addEventListener('input', () => {
-    if (!selected) return console.error("error: selected is not defined");
-    memoData[selected.getAttribute('name')] = popup_memo.value;
-    localStorage.setItem('memo', JSON.stringify(memoData));
-});
+    // 入力時保存処理
+    popup_memo.addEventListener('input', () => {
+        if (!selected) return console.error("error: selected is not defined");
+        memoData[selected.getAttribute('name')] = popup_memo.value;
+        localStorage.setItem('memo', JSON.stringify(memoData));
+    });
+})();
